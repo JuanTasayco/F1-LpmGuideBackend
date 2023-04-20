307a0ad804c19a1cf56e6155496f23279363366d
JuanTasayco <juantasayco266@gmail.com>
2023-04-20 14:46:02 -0500

    addRouteToFileCloud

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -8,10 +8,15 @@ export class CloudinaryService {
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream((error, result) => {
-        if (error) return reject(error);
-        resolve(result);
-      });
+      const upload = v2.uploader.upload_stream(
+        {
+          folder: 'lpm',
+        },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
       toStream(file.buffer).pipe(upload);
     });
   }
@@ -21,11 +26,16 @@ export class CloudinaryService {
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     base64Data = base64Data.replace(/^data:image\/\w+;base64,/, '');
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream((error, result) => {
-        if (error) return reject(error);
+      const upload = v2.uploader.upload_stream(
+        {
+          folder: 'lpm',
+        },
+        (error, result) => {
+          if (error) return reject(error);
 
-        resolve(result);
-      });
+          resolve(result);
+        },
+      );
       const stream = streamifier.createReadStream(
         Buffer.from(base64Data, 'base64'),
       );
