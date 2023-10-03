0ca2cfec3160ad7a1103124399ebb83941ddad54
JuanTasayco <juantasayco266@gmail.com>
2023-10-02 19:51:23 -0500

    addCredentialsDeployFl0

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,27 +6,26 @@ import { SeedModule } from './seed/seed.module';
 import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
-
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: "postgres",
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true,
-      autoLoadEntities: true
-
+      autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     LpmModule,
     SeedModule,
     AuthModule,
     CloudinaryModule,
-],
+  ],
 })
-export class AppModule { }
-
+export class AppModule {}
